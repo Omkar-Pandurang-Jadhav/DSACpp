0ea4c6b266e10bf2fb2bbd3b03d0301f6a65f854
Omkar Jadhav <162875896+Omkar-Pandurang-Jadhav@users.noreply.github.com>
2024-07-11 11:15:27 +0530

    Update STL2.cpp

--- Step1: Learn The Basics/step 1.3 c++ STL/STL2.cpp
+++ Step1: Learn The Basics/step 1.3 c++ STL/STL2.cpp
@@ -36,7 +36,7 @@ void explainStack(){
     st.push(1);//{1}
     st.push(2);//{2,1}
     st.push(3);//{3,2,1}
-    st.push(4);//{3,3,2,1}
+    st.push(3);//{3,3,2,1}
     st.emplace(5);//{5,3,3,2,1} // similar to push
 
      cout<<st.top(); // prints 5 "** st[2] is invalid **"
